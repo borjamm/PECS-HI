15b3e9d7f77311554782c34b61dccb8762e98b1b
borjamm <codebmmjob@gmail.com>
2020-11-13 23:00:56 +0000

    Changes class CSS

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-
-
 window.addEventListener('load', () => {
-    console.log("Cargado");
-
-
     const pathname = window.location.pathname;
     if (pathname.includes('details')) {
-
-        console.log("Entramos");
         const linkShowMore = document.getElementById('show-more');
         const descriptionMoreInformation = document.getElementById('show-description');
 
         linkShowMore.addEventListener('click', () => {
             descriptionMoreInformation.classList.toggle('open');
             if(linkShowMore && linkShowMore.textContent === 'Ver menos') {
-                while(linkShowMore.firstChild){ linkShowMore.removeChild(linkShowMore.firstChild); }
+                while(linkShowMore.firstChild){ 
+                    linkShowMore.removeChild(linkShowMore.firstChild); 
+                }
                 linkShowMore.textContent = 'Ver más';
                 const i = document.createElement('i');
                 i.classList.add('arrow');
                 i.classList.add('down');
                 linkShowMore.append(i);
             } else {
-                while(linkShowMore.firstChild) { linkShowMore.removeChild(linkShowMore.firstChild); }
+                while(linkShowMore.firstChild) { 
+                    linkShowMore.removeChild(linkShowMore.firstChild); 
+                }
                 linkShowMore.textContent = 'Ver menos';
                 const i = document.createElement('i');
                 i.classList.add('arrow');
